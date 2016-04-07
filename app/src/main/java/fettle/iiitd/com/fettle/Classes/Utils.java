37810eaf478ead4d9a88376c5a4cac139474bdaa
MnnGkhr <manang168@gmail.com>
2016-04-08 01:05:48 +0530

    global Shared pref

--- app/src/main/java/fettle/iiitd/com/fettle/Classes/Utils.java
+++ app/src/main/java/fettle/iiitd/com/fettle/Classes/Utils.java
@@ -21,6 +21,7 @@ public class Utils {
     public static final String DAILY_CALORIE_KEY = "dailyCalorie";
     public static final String WALK_10_CALOIRES_KEY = "walkTen";
     public static final String RUN_10_CALOIRES_KEY = "runTen";
+    public static final String PREF_NAME = "Pref";
 
     public static float getBmi(int heightInCm, int weight) {
         float height = heightInCm / 100f;
@@ -108,26 +109,26 @@ public class Utils {
     }
 
     public static void storePref(Activity activity, String key, String value) {
-        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putString(key, value);
         editor.commit();
     }
 
     public static String getPref(Activity activity, String key) {
-        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         return sharedPref.getString(key, "0");
     }
 
     public static void storePref(Activity activity, String key, int value) {
-        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt(key, value);
         editor.commit();
     }
 
     public static int getPref(String key, Activity activity) {
-        SharedPreferences sharedPref = activity.getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences sharedPref = activity.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
         return sharedPref.getInt(key, 0);
     }
 
